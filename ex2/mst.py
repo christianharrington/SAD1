f195c37987f6583b6597a8c12677e24777d82a31
Christian Harrington <christian@harrington.dk>
2012-09-12 11:00:57 +0200

    Works now

--- ex2/mst.py
+++ ex2/mst.py
@@ -9,8 +9,8 @@ class Node:
 	def __init__(self, n):
 		self.id = n
 		self.edges = {}
-		self.parent
-		self.rank
+		self.parent = self
+		self.rank = 0
 	
 class Edge:
 	def __init__(self, node1, node2, weight):
@@ -21,6 +21,10 @@ class Edge:
 	def __str__(self):
 		return str(self.weight)
 
+# Loads a file. Nodes and edges are as follows
+# Nodes: One node per line. The string representing the node will be used as an ID, and must be unique. If the string has spaces, the string must be encapsualted in qoutes.
+# Edges: One edge per line, written as follows: "Node 1"--Node2 [weight]
+# The nodes specified in the edge must be declared first as described above.
 def loadFile():
 	with open(sys.argv[1], 'r') as file:
 		for line in file:
@@ -32,23 +36,17 @@ def loadFile():
 				node1 = s[0]
 				node2 = s[1][:s[1].find("[")].strip()
 				weight = s[1][s[1].find("[")+1:].strip().strip("]")
-		
+				
 				road = Edge(nodes[node1], nodes[node2], int(weight))
 				edges.append(road)
-		
+				
 				nodes[node1].edges[node2] = road
 				nodes[node2].edges[node1] = road
-
-				print node1, '-', node2, ':', weight
 
 def find(node):
 	if node.parent != node:
 		node.parent = find(node.parent)
 	return node.parent
-				
-def makeSet(node):
-     node.parent = node
-     node.rank   = 0
 
 def union(node1, node2):
 	root1 = find(node1)
@@ -59,7 +57,7 @@ def union(node1, node2):
 	
 	if root1.rank > root2.rank:
 		root2.parent = root1
-	else if root1.rank < root2.rank:
+	elif root1.rank < root2.rank:
 		root1.parent = root2
 	else:
 		root2.parent = root1
@@ -67,10 +65,14 @@ def union(node1, node2):
 
 def kruskal(nodes, edges):
 	queue = list(edges)
+	weight = 0
 	while queue:
 		edge = queue.pop(0)
-		print edge
-		# INSERT ALGORITHM HERE
+		if find(edge.node1) != find(edge.node2):
+			union(edge.node1, edge.node2)
+			count += 1
+			weight += edge.weight
+	print weight
 
 loadFile()
 edges.sort(cmp=lambda x,y:cmp(x.weight, y.weight))
