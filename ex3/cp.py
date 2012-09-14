651188eb7ab77d05ad720a6e2ae838aa146e0611
Christian Harrington <christian@harrington.dk>
2012-09-14 15:32:44 +0200

    Implemented ugly parsing

--- ex3/cp.py
+++ ex3/cp.py
@@ -3,6 +3,7 @@
 # Christian Harrington & Nicolai Dahl
 
 import sys
+import os
 
 points = []
 
@@ -11,9 +12,24 @@ class Point:
 		self.x = x
 		self.y = y
 
-def loadFile():
-	print "Parse"
+def loadFile(file):
+	wait = False
+	
+	for line in file:
+		if "NAME" in line:
+			wait = True
+		elif "NODE_COORD_SECTION" in line:
+			wait = False
+		elif line.strip() and wait == False and not "EOF" in line:
+			s = line.strip().split(" ")
+			points.append(Point(float(s[1]), float(s[2])))
 
-def closestPath():
+def closestPath(points):
 	print "closestPath"
 
+for file in sys.argv:
+	if file != sys.argv[0] and os.path.exists(file):
+		print "Parsing", file
+		points = []
+		loadFile(open(file, 'r'))
+		closestPath(points)
