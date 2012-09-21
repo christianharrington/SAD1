85c10f1f3deafe39b1b65bc443cee1bf151643be
Christian Harrington <christian@harrington.dk>
2012-09-21 14:39:37 +0200

    Implemented parser

--- ex4/sa.py
+++ ex4/sa.py
@@ -1,3 +1,23 @@
 #!/usr/bin/env python
 
 # Christian Harrington & Nicolai Dahl
+
+import sys
+
+def loadFile(file):
+	seqs = {}
+	
+	for line in file:
+		line = line.strip()
+		if line[0] == '>':
+			name = line[1:].split(' ')[0]
+			seqs[name] = ''
+		else:
+			seqs[name] += line
+			
+	return seqs
+	
+def seqAlg(seqs):
+	return seqs
+			
+print seqAlg(loadFile(open(sys.argv[1], 'r')))
