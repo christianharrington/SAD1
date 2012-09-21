e24ed2c10f5b5dedab0ff70203ea24f8945f2654
Christian Harrington <christian@harrington.dk>
2012-09-21 16:04:27 +0200

    Can load the score table now

--- ex4/sa.py
+++ ex4/sa.py
@@ -3,8 +3,37 @@
 # Christian Harrington & Nicolai Dahl
 
 import sys
+import re
 
-def loadFile(file):
+# Loads the score table
+def loadScores(file):
+	scores = {}
+	scoreOrder = []
+	
+	indicesPattern = re.compile('[A-Z*]')
+	scoresPattern = re.compile('\-?[0-9]+')
+	
+	for line in file:
+		line = line.strip()
+		if line[0] != '#' and not scores:
+			indices = indicesPattern.findall(line)
+			
+			for i in indices:
+				scores[i] = {}
+				scoreOrder.append(i)
+		
+		elif line[0] != '#':
+			n = 0
+			scoreMatches = scoresPattern.findall(line[1:])
+
+			for s in scoreMatches:
+				scores[line[0]][scoreOrder[n]] = s
+				n += 1
+
+	return scores			
+
+# Loads the sequences
+def loadSequence(file):
 	seqs = {}
 	
 	for line in file:
@@ -16,18 +45,22 @@ def loadFile(file):
 			seqs[name] += line
 			
 	return seqs
-	
+
+# The algorithm
+def opt(x, i, y, j):
+	penalty = scores[x[i]][y[j]]
+	max(penalty + opt(x, i - 1, y, j - 1), max(penalty + opt(x, i - 1, y, j, opt(x, i, y, j - 1))))
+
 def alignment(x, y):
 	penaltyArray = []
 	
 	for i in range(0, len(x)):
 		penaltyArray.append([max(len(x), len(y)) for item in range(0, len(y))])
-	
-	
-	
-def seqAlg(seqs):
-	return seqs
-			
-print seqAlg(loadFile(open(sys.argv[1], 'r')))
 
-print alignment('hej', 'ehje')
+
+#print alignment('hej', 'ehje')
+
+scores = loadScores(open(sys.argv[1], 'r'))
+seqs = loadSequence(open(sys.argv[2], 'r'))
+
+for s in 
