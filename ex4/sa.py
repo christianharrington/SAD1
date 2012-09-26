dc1ac4a78195a2f7d55484edf923b5b63bfd4b9c
Christian Harrington <christian@harrington.dk>
2012-09-26 15:54:44 +0200

    Compares results

--- ex4/sa.py
+++ ex4/sa.py
@@ -32,8 +32,7 @@ def loadScores(file):
             for s in scoreMatches:
                 scores[line[0]][scoreOrder[n]] = float(s)
                 n += 1
-
-    
+                 
     return scores            
 
 # Loads the sequences
@@ -49,60 +48,99 @@ def loadSequence(file):
             seqs[name] += line
             
     return seqs
+    
+# Compare results
+def loadCompare(cfile):
+    compareResults = []
+    
+    for line in cfile:
+        line = line.strip()
+        if ':' in line:
+            (compare1, t1) = line.split('--')
+            (compare2, r) = t1.split(': ')
+            
+            result = (compare1, compare2, r, None, None)
+        else:
+            if result[3] == None:
+                result = (result[0], result[1], result[2], line.strip(), None)
+            elif result[4] == None:
+                result = (result[0], result[1], result[2], result[3], line.strip())
+                compareResults.append(result)
+                
+    return compareResults
 
 # The algorithm
 def opt(x, i, y, j):
-    
-        if i == 0 and j == 0:
-            penalty = scores[x[i]][y[j]]
-            if penalty < delta:
-                return (delta, '-', y[j])
-            else:
-                return (penalty, x[i], y[j])
-        elif i < 0:
-            return (delta * (j + 1), '-' * (j + 1), y[0:j + 1])
-        elif j < 0:
-            return (delta * (i + 1), x[0:i + 1], '-' * (i + 1))
+    if i == 0 and j == 0:
+        penalty = scores[x[i]][y[j]]
+        if penalty < delta:
+            return (delta, '-', y[j])
         else:
-            if penaltyArray[i][j][0] is None:
-                penalty = scores[x[i]][y[j]]
-                first = opt(x, i - 1, y, j - 1)
-                snd = opt(x, i - 1, y, j)
-                third = opt(x, i, y, j - 1)
+            return (penalty, x[i], y[j])
+    elif i < 0:
+        return (delta * (j + 1), '-' * (j + 1), y[0:j + 1])
+    elif j < 0:
+        return (delta * (i + 1), x[0:i + 1], '-' * (i + 1))
+    else:
+        if penaltyArray[i][j][0] is None:
+            penalty = scores[x[i]][y[j]]
+            first = opt(x, i - 1, y, j - 1)
+            snd = opt(x, i - 1, y, j)
+            third = opt(x, i, y, j - 1)
                 
-                maxval = (penalty + first[0], first[1] + x[i], first[2] + y[j])
+            maxval = (penalty + first[0], first[1] + x[i], first[2] + y[j])
                         
-                if delta + snd[0] > maxval[0]:
-                    maxval = (delta + snd[0], snd[1] + x[i], snd[2] + '-')
+            if delta + snd[0] > maxval[0]:
+                maxval = (delta + snd[0], snd[1] + x[i], snd[2] + '-')
                     
-                if delta + third[0] > maxval[0]:
-                    maxval = (delta + third[0], third[1] + '-' , third[2] + y[j])
+            if delta + third[0] > maxval[0]:
+                maxval = (delta + third[0], third[1] + '-' , third[2] + y[j])
                 
-                return maxval
-            else:
-                return penaltyArray[i][j]
+            return maxval
+        else:
+            return penaltyArray[i][j]
         
 def alignment(x, y):
     for _ in range(0, len(x)):
-        penaltyArray.append([(None, '', '') for _ in range(0, len(y))])
+        penaltyArray.append([(None, '', '') for i in range(0, len(y))])
 
-    penaltyArray[len(x) - 1][len(y) - 1] = opt(x, len(x) - 1, y, len(y) - 1)
+    for i in range(0, len(x)):
+        for j in range(0, len(y)):
+            penaltyArray[i][j] = opt(x, i, y, j)
+        
     return penaltyArray[len(x) - 1][len(y) - 1]
-
-
-scores = loadScores(open('BLOSUM62.txt', 'r'))
+    
+# Load files and run the algorithm./sa.py data/BLOSUM62.txt data/HbB_FASTAs.in data/HbB_FASTAs.out
+scores = loadScores(open(sys.argv[1], 'r'))
 delta = scores['*']['A']
-print delta
-print alignment('KAK', 'KQRIKAAKABK')
+seqs = loadSequence(open(sys.argv[2], 'r'))
+compareResults = loadCompare(open(sys.argv[3], 'r'))
 
-#seqs = loadSequence(open(sys.argv[2], 'r'))
+incorrect = 0
 
-
-
-
-
-
-
-
-
-
+for s in compareResults:
+    penaltyArray = []
+    
+    v1 = seqs[s[0]]
+    v2 = seqs[s[1]]
+    
+    sa = alignment(v1, v2)
+    
+    print '{}--{}: {}/{}'.format(s[0], s[1], s[2], sa[0])
+    
+    print s[3]
+    print s[4]
+    print 
+    print sa[1]
+    print sa[2]
+    print
+    
+    if sa[0] == float(s[2]):
+        print 'CORRECT'
+    else:
+        print 'INCORRECT'
+        incorrect += 1
+    
+    print
+    
+print 'Incorrect results: {}\n'.format(incorrect)
