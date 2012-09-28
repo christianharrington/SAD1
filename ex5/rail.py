4a9041859d0a46f7559162b692c9ab668dd85064
Nicolai Dahl <ndahl89@gmail.com>
2012-09-28 14:44:57 +0200

    Added data structure

--- ex5/rail.py
+++ ex5/rail.py
@@ -1,3 +1,21 @@
 #!/usr/bin/env python
 
 # Christian Harrington & Nicolai Dahl
+
+class Graph:
+	def __init__(self, vertices, edges):
+		self.vertices = vertices
+		self.edges = edges
+
+class Node:
+	def __init__(self, n):
+		self.id = n
+		self.edges = []
+		
+class Edge:
+	def __init__(self, from, to, capacity, flow):
+		self.from = from
+		self.to = to
+		self.capacity = capacity
+		self.flow = flow
+
