bbb9130a7a4ae9fce2ca3d678505a494faaed706
Nicolai Dahl <ndahl89@gmail.com>
2012-10-02 14:18:14 +0200

    Corrected the bottleneck function

--- ex5/rail.py
+++ ex5/rail.py
@@ -3,92 +3,90 @@
 # Christian Harrington & Nicolai Dahl
 
 import sys
+import collections
 
 class Graph:
-	def __init__(self, nodes, edges):
-		self.nodes = nodes
-		self.edges = edges
-
+    def __init__(self, nodes, edges):
+        self.nodes = nodes
+        self.edges = edges
+    
 class Node:
-	def __init__(self, i, name):
-		self.id = i
-		self.name = name
-		self.edges = []
-		
-	def __str__(self):
-		return str(self.id) + ': ' + self.name
-		
+    def __init__(self, i, name):
+        self.id = i
+        self.name = name
+        self.edges = []
+        
+    def __str__(self):
+        return str(self.id) + ': ' + self.name
+        
 class Edge:
-	def __init__(self, fromNode, toNode, capacity, flow):
-		self.fromNode = fromNode
-		self.toNode = toNode
-		self.capacity = capacity
-		self.flow = flow
-		
-	def __str__(self):
-		return str(self.fromNode) + ' ' + str(self.toNode) + ' ' + str(self.capacity)
+    def __init__(self, fromNode, toNode, capacity, flow):
+        self.fromNode = fromNode
+        self.toNode = toNode
+        self.capacity = capacity
+        self.flow = flow
+        
+    def __str__(self):
+        return str(self.fromNode) + ' ' + str(self.toNode) + ' ' + str(self.capacity)
 
 def loadData(railFile):
-	i = 0
-	nodes = []
-	edges = []
-	
-	for line in railFile:
-		lint = line.strip()
-		
-		if i == 0:
-			n = int(line)
-		elif i <= n:
-			nodes.append(Node(i - 1, line))
-		elif i == n + 1:
-			m = int(line)
-		else:
-			(fromNode, toNode, cap) = line.split(' ')
-			
-			edge = Edge(int(fromNode), int(toNode), int(cap), 0)
-			edges.append(edge)
-			nodes[int(fromNode)].edges.append(edge)
-			nodes[int(toNode)].edges.append(edge)
-			
-		i += 1
-		
-	return Graph(nodes, edges)
-	
+    i = 0
+    nodes = []
+    edges = []
+    
+    for line in railFile:
+        line = line.strip()
+        
+        if i == 0:
+            n = int(line)
+        elif i <= n:
+            nodes.append(Node(i - 1, line))
+        elif i == n + 1:
+            m = int(line)
+        else:
+            (fromNode, toNode, cap) = line.split(' ')
+            
+            edge = Edge(int(fromNode), int(toNode), int(cap), 0)
+            edges.append(edge)
+            nodes[int(fromNode)].edges.append(edge)
+            nodes[int(toNode)].edges.append(edge)
+            
+        i += 1
+        
+    return Graph(nodes, edges)
+    
 graph = loadData(open(sys.argv[1], 'r'))
 
 
 prevDict = {}
 
-def bottleneck(path):
-	if len(path) == 1:
-		return path[0].capacity
-	else:
-		e = path.pop[0]
-		return max(bottleneck(path), e.capacity)
+def bottleneck(path, flow):
+    return min([e.capacity for e in path]) - flow
+
 
 def backtrackPath(source, tempNode):
-	if source == tempNode:
-		return source
-	else:
-		return backtrackPath(source, prevDict[tempNode.id]).insert(0, tempNode)
+    if source == tempNode:
+        return source
+    else:
+        return backtrackPath(source, prevDict[tempNode.id]).insert(0, tempNode)
 
 def bfs(source, sink):
-	prevDict = {}
-	visitedDict = {}
+    prevDict = {}
+    visitedDict = {}
 
-	q = deque()
-	q.append(source)
-	
-	while(not q.empty()):
-		t = q.popleft()
+    q = collections.deque()
+    q.append(source)
+    
+    while(not q.empty()):
+        t = q.popleft()
 
-		if t == sink:
-			return backtrackPath(source, t)
-		else:
-			for edge in t.edges:
-				prevDict[edge.toNode] = edge.fromNode
-				visitedDict[edge.fromNode] = True
-				q.append(edge)
-	
-	
+        if t == sink:
+            return backtrackPath(source, t)
+        else:
+            for edge in t.edges:
+                prevDict[edge.toNode] = edge.fromNode
+                visitedDict[edge.fromNode] = True
+                q.append(edge)
+    
+    
 
