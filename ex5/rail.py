f60c49121e9603d7d7352df35ea637149e17b6fc
Nicolai Dahl <ndahl89@gmail.com>
2012-10-02 13:17:01 +0200

    Added a simple breadth first search (not tested)

--- ex5/rail.py
+++ ex5/rail.py
@@ -2,6 +2,8 @@
 
 # Christian Harrington & Nicolai Dahl
 
+from collections import deque
+
 class Graph:
 	def __init__(self, vertices, edges):
 		self.vertices = vertices
@@ -13,9 +15,34 @@ class Node:
 		self.edges = []
 		
 class Edge:
-	def __init__(self, from, to, capacity, flow):
-		self.from = from
-		self.to = to
+	def __init__(self, fromNode, toNode, capacity, flow):
+		self.fromNode = fromNode
+		self.toNode = toNode
 		self.capacity = capacity
 		self.flow = flow
 
+def backtrackPath(source, tempNode):
+	if source == tempNode:
+		return source
+	else:
+		return backtrackPath.insert(0, tempNode)
+
+def bfs(source, sink):
+	prevDict = {}
+	visitedDict = {}
+
+	q = deque()
+	q.append(source)
+	
+	while(not q.empty()):
+		t = q.popleft()
+
+		if t == sink:
+			return backtrackPath(source, t)
+		else:
+			for edge in t.edges:
+				prevDict(edge.toNode) = edge.fromNode
+				visitedDict(edge.fromNode) = True
+				q.append(edge)
+	
+	
